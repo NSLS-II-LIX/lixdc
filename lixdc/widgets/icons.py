b89ed62c37eecd40290545a856715a5646efc2f7
Arman Arkilic <armanarkilic@gmail.com>
2016-06-29 12:10:23 -0400

    Revert "WIP: Windowless Setup"

--- lixdc/widgets/icons.py
+++
@@ -1,4 +0,0 @@
-CLOSE = ":/trolltech/styles/commonstyle/images/standardbutton-closetab-16.png"
-ADD = ":/trolltech/styles/commonstyle/images/newdirectory-128.png"
-FIND = ":/trolltech/styles/commonstyle/images/filecontents-32.png"
-IMPORT = ":/trolltech/styles/commonstyle/images/parentdir-128.png"
--- lixdc/widgets/tab_flowmixer_setup.py
+++
@@ -1,7 +0,0 @@
-from PyQt4 import QtGui
-
-
-class FlowMixerSetup(QtGui.QWidget):
-    
-    def __init__(self):
-        super(FlowMixerSetup, self).__init__()
